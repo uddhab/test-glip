7a8a789bbd1a7fca4dd81e14e7184dd16960a7db
uddhab <uddhab@crossovernepal.com>
2014-11-03 14:57:34 +0545

    initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Initialization & includes {{{1
+error_reporting(-1);
+
+require_once "vendor/united-asian/glip/lib/glip.php";
+
+$repo = Admin::init("home/n5110/projects/polifonic/test-glip/test-repo");
